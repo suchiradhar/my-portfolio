557e61345ff817a4d361df680f5b177001c438ee
Suchira Dhar <suchiradhar@Suchiras-MacBook-Pro-5.local>
2024-10-30 17:01:24 +0000

    fghfh

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,7 +5,6 @@ import projectThree from '../../assets/projectThree.jpg';
 import projectFour from '../../assets/projectFour.jpg';
 
 import './Projects.css';
-import "../../App.css";
 
 const ProjectCard = ({ image, title, subtitle, link }) => (
   <a href={link} target="_blank" rel="noopener noreferrer" className="project__card">
